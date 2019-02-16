3b203f1d93fcb5a12ef87561e56adb539a58d617
lalu <laluvjohn@gmail.com>
2019-02-16 15:31:50 -0600

    New branch to setup base page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './components/map/unemployment_us_map.css';
 import Map from "./components/map/map";
+import {Jumbotron, Container, Button} from 'reactstrap';
+import styled from 'styled-components';
 
 class App extends Component {
   render() {
     return (
         <div>
-            <Map />
+           <Jumbotron>
+               <Map />
+           </Jumbotron>
+
         </div>
     );
   }
