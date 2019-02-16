121019f22ebecacf298486f34ffeb57400d8774b
stephen <jonesstephen239@gmail.com>
2019-02-16 14:15:48 -0600

    poc of map and updated gitignore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './components/map/unemployment_us_map.css';
+import Map from "./components/map/map";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            <div id={"map"}></div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+            <Map />
+        </div>
     );
   }
 }
