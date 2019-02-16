cd9f8cc6e2063b4bfb97451713148d501c002cc5
Andrew Veatch <ahveatch@gmail.com>
2019-02-16 17:24:15 -0600

    Added csv data to scatter plot and added formatting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Scatterplot from "./components/scatter-plot/scatter-plot.js"
+import ScatterPlot from "./components/scatter-plot/scatter-plot.js"
 import logo from './logo.svg';
 import './components/map/unemployment_us_map.css';
 import Map from "./components/map/map";
@@ -9,8 +9,7 @@ class App extends Component {
   render() {
     return (
       <div >
-        <Scatterplot />
-        <Map />
+        <ScatterPlot />
       </div>
     );
   }
--- src/components/scatter-plot/scatter-plot.js
+++ src/components/scatter-plot/scatter-plot.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+import data from '../../data/unemployment_income_2007_to_2017.csv';
 
-class Scatterplot extends Component{
+class ScatterPlot extends Component{
 
     constructor(props){
         super(props);
@@ -19,70 +20,32 @@ class Scatterplot extends Component{
 
     createScatterPlot(){
         //width and height
-		var w = 600;
-		var h = 400;
-		var padding = 40;
-		
-		/*
-		var dataset;
-		//load data
-		d3.csv('weather.csv', function(d) {
-			//data.forEach(function(d) {
-			//dataset.push(d.precipitation);
+		var w = 1000;
+		var h = 600;
+		var padding = 75;
+		var i = 0;
+		d3.csv(data, (row) => {
 			return {
-				//max: d.Maximum,
-				//min: d.Minimum,
-				//month: d.Month,
-				precipitation: d.Precipitation
-				//speed: d.Speed,
-				//state: d.State,
-				//year: d.Year
-			};
-		}, function(d) {
-			dataset.push(d.precipitation); 
-		});
-		
-		
-		
-		var temp;
-		var string;
-		for(var i = 0; i < numBars; i++) {
-			string = dataset.get(i);
-			state = states.get(i);
-			temp = {state: string};
-			dataset.push(temp);
-		}*/
-		
-		//var dataset = data.slice(0, numBars + 1);
-		//max vs min
-		var dataset = [];
-		dataset.push([46, 32]);
-		dataset.push([47, 31]);
-		dataset.push([51, 41]);
-		dataset.push([52, 38]);
-		dataset.push([38, 29]);
-		
-		/*
-		dataset = [{max: 46, min: 32},
-					{max: 47, min: 31},
-					{max: 51, min: 41},
-					{max: 52, min: 38},
-					{max: 38, min: 29}];
-					*/
-		
-		//scale function
-		var xScale = d3.scaleLinear()
-			//.domain(["Alabama","Alaska","Arizona","Arkansas","California"])
-			.domain([0, d3.max(dataset, function(d) { return d[0]; })])
-			//.range([padding, w-padding * 2]);
+				county_code: +row['FIPStxt'],
+				state: row['State'],
+				employed: +row['Employed_2016'].replace(/[^0-9.-]+/g, ""),
+				median_income: +row['Median_Household_Income_2016'].replace(/[^0-9.-]+/g, "")
+			}
+		}
+		).then(data => {
+			
+			data = data.filter(d => (d.county_code % 1000 == 0))
+			data = data.filter(d => !(isNaN(d.employed) || isNaN(d.median_income) || d.median_income === 0))
+			
+			var xScale = d3.scaleLinear()
+			.domain(d3.extent(data, function(d) { return d.median_income; }))
 			.range([padding, w - padding * 2]);
 			
 		var yScale = d3.scaleLinear()
-			.domain([0, d3.max(dataset, function(d) { return d[1]; })])
-			//.range([padding, w-padding * 2]);
+			.domain(d3.extent(data, function(d) { return d.employed; }))
 			.range([h - padding, padding]);
 		
-		var xAxis = d3.axisBottom().scale(xScale).ticks(5);
+		var xAxis = d3.axisBottom().scale(xScale);
 		
 		var yAxis = d3.axisLeft().scale(yScale).ticks(5);
 		
@@ -92,19 +55,8 @@ class Scatterplot extends Component{
 					.attr("width", w)
 					.attr("height", h);
 					
-		svg.selectAll("circle")
-			.data(dataset)
-			.enter()
-			.append("circle")
-			.attr("cx", function(d) {
-				return xScale(d[0]);
-			})
-			.attr("cy", function(d) {
-				return h - yScale(d[1]);
-			})
-			.attr("r", 5)
-			.attr("fill", "green");
-			
+		
+		console.log(data);
 		//x axis
 		svg.append("g")
 			.attr("class", "x axis")	
@@ -116,20 +68,65 @@ class Scatterplot extends Component{
 			.attr("class", "y axis")	
 			.attr("transform", "translate(" + padding + ", 0)")
 			.call(yAxis);
+
+		
+
+        //    .attr(“class”, “dot”)
+        //    .attr(“r”, function (d) {
+        //        return d.r;
+        //    })
+        //    .attr(“cx”, function (d) {
+        //        return x(d.x);
+        //    })
+        //    .attr(“cy”, function (d) {
+        //        return y(d.y);
+        //    })
+        //    .style(“fill”, function (d) {
+        //        return d.c;
+        //    });
+        //    gdots.append(“text”).text(function(d){
+        //        return d.name;
+        //    })
+        //    .attr(“x”, function (d) {
+        //        return x(d.x);
+        //    })
+        //    .attr(“y”, function (d) {
+        //        return y(d.y);
+        //    });
 			
+			var circle = svg.selectAll("circle")
+			.data(data)
+			.enter()
+			.append("g")
+			circle.append("circle")
+			.attr("class", "dot")
+			.attr("cx", function(d) {
+				return xScale(d.median_income);
+			})
+			.attr("cy", function(d) {
+				return yScale(d.employed);
+			})
+			.on("mouseover", (d) => console.log(d))
+			.attr("r", 20)
+			.attr("fill", "green")
+			.attr("opacity", .5)
+
+			circle.append("text")
+			.attr("x", (d) => xScale(d.median_income))
+			.attr("y", (d) => yScale(d.employed) + 5)
+			.text((d) => d.state) 
+			.attr("text-anchor", "middle") 
+		
+			
+	
+		});
     }
-
-
+	
     render(){
         return(
             <svg ref={node => this.node = node} width={400} height={400}></svg>
         );
     }
-
-
-
-
 }
 
-
-export default Scatterplot
+export default ScatterPlot
