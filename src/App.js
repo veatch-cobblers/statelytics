d10f6120f87c25af15dbf2418821018b22758648
stephen <jonesstephen239@gmail.com>
2019-02-16 13:32:22 -0600

    added datasets

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+            <div id={"map"}></div>
           <a
             className="App-link"
             href="https://reactjs.org"
---
+++ src/components/map/unemployment_us_map.js
@@ -0,0 +1,78 @@
+import d3 from 'd3';
+import topojson from 'topojson';
+
+let width=960;
+let height=600;
+
+var svg = d3.select("#map"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+var unemployment = d3.map();
+
+var path = d3.geoPath();
+
+var x = d3.scaleLinear()
+    .domain([1, 10])
+    .rangeRound([600, 860]);
+
+var color = d3.scaleThreshold()
+    .domain(d3.range(2, 10))
+    .range(d3.schemeBlues[9]);
+
+var g = svg.append("g")
+    .attr("class", "key")
+    .attr("transform", "translate(0,40)");
+
+g.selectAll("rect")
+    .data(color.range().map(function(d) {
+        d = color.invertExtent(d);
+        if (d[0] == null) d[0] = x.domain()[0];
+        if (d[1] == null) d[1] = x.domain()[1];
+        return d;
+    }))
+    .enter().append("rect")
+    .attr("height", 8)
+    .attr("x", function(d) { return x(d[0]); })
+    .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+    .attr("fill", function(d) { return color(d[0]); });
+
+g.append("text")
+    .attr("class", "caption")
+    .attr("x", x.range()[0])
+    .attr("y", -6)
+    .attr("fill", "#000")
+    .attr("text-anchor", "start")
+    .attr("font-weight", "bold")
+    .text("Unemployment rate");
+
+g.call(d3.axisBottom(x)
+    .tickSize(13)
+    .tickFormat(function(x, i) { return i ? x : x + "%"; })
+    .tickValues(color.domain()))
+    .select(".domain")
+    .remove();
+
+var promises = [
+    d3.json("https://d3js.org/us-10m.v1.json"),
+    d3.tsv("unemployment.tsv", function(d) { unemployment.set(d.id, +d.rate); })
+]
+
+Promise.all(promises).then(ready)
+
+function ready([us]) {
+    svg.append("g")
+        .attr("class", "counties")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.counties).features)
+        .enter().append("path")
+        .attr("fill", function(d) { return color(d.rate = unemployment.get(d.id)); })
+        .attr("d", path)
+        .append("title")
+        .text(function(d) { return d.rate + "%"; });
+
+    svg.append("path")
+        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+        .attr("class", "states")
+        .attr("d", path);
+}
