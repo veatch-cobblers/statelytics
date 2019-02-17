21bdd1e1de7599f1a9bf09b02175e82a08457015
lalu <laluvjohn@gmail.com>
2019-02-17 07:24:31 -0600

    fixed lint

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -30,9 +30,6 @@ class Map extends Component {
         select(node).selectAll('*').remove();
         var path = d3.geoPath();
 
-        var x = d3.scaleLinear()
-            .domain([1, 10])
-            .rangeRound([600, 860]);
 
         let rankingMetric = d3.map();
         Promise.all([
